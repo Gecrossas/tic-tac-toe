156e92881671dfa9efb199a1751b7f7a790fbc65
Gediminas <115144042+Gecrossas@users.noreply.github.com>
2023-10-15 17:49:47 +0300

    Add game controller and improved separation of concerns

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 const X = "X";
 const O = "O";
-
-let currentPiece = X;
+const boardElement = document.querySelector(".board");
 
 const boardController = (function () {
-    const board = [
-        X, O, X,
-        "", O, "",
-        "", "", O
+    const _board = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
     ];
 
     function addPiece(piece, coordinate) {
-        if (board[coordinate] === "") {
-            board[coordinate] = piece;
+        if (piece != X && piece != O) 
+            return false;
+
+        if (_board[coordinate] === "") {
+            _board[coordinate] = piece;
+            return true;
         }
+        return false;
     };
 
     function getBoard() {
-        return board;
+        return _board;
     };
 
     return { addPiece, getBoard };
 })();
 
 const displayController = (function () {
-    const boardElement = document.querySelector(".board");
+    let _boardElement;
 
-    function createCellElement(piece, index) {
+    function init(boardElement) {
+        _boardElement = boardElement;
+    }
+
+    function _createCellElement(piece, index) {
         const element = document.createElement("div");
         element.setAttribute("data-index", index)
         element.textContent = piece;
@@ -41,35 +49,66 @@ const displayController = (function () {
         return element;
     }
 
-    function clearBoard() {
-        let children = Array.from(boardElement.children);
+    function _clearBoard() {
+        let children = Array.from(_boardElement.children);
         children.forEach(child => {
             child.remove();
         })
     }
 
     function render(board) {
-        clearBoard();
+        _clearBoard();
         let index = 0;
         board.forEach(cell => {
-            boardElement.appendChild(createCellElement(cell, index));
+            _boardElement.appendChild(_createCellElement(cell, index));
             index++;
         });
     }
 
-    return { render };
+    function getBoardElement() {
+        return _boardElement;
+    }
+
+    return { init, render };
 })();
 
-displayController.render(boardController.getBoard());
+const gameController = (function () {
+    let _currentPiece;
+    let _boardElement;
 
-const boardElement = document.querySelector(".board");
-boardElement.addEventListener("click", (event) => {
-    if (event.target.classList.contains("cell")) {
-        let index = event.target.getAttribute("data-index")
-        boardController.addPiece(currentPiece, index);
-        displayController.render(boardController.getBoard());
+    function init(startingPiece, boardElement) {
+        _currentPiece = startingPiece;
+        _boardElement = boardElement;
+        _boardElement.addEventListener("click", (cell) => {
+            if (cell.target.classList.contains("cell")) {
+                _renderPiece(cell);
+            }
+        });
     }
-});
+
+    function _renderPiece(cellElement) {
+        let index = cellElement.target.getAttribute("data-index")
+        let success = boardController.addPiece(_currentPiece, index);
+        displayController.render(boardController.getBoard());
+        if (success) _switchPiece();
+    }
+
+    function _switchPiece() {
+        _currentPiece = (_currentPiece === X) ? O : X;
+    }
+
+    function getCurrentPiece() {
+        return _currentPiece;
+    }
+
+    return { init, getCurrentPiece };
+})();
+
+displayController.init(boardElement);
+displayController.render(boardController.getBoard());
+gameController.init(X, boardElement);
+
+
 
 
 
